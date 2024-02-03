ad59dbb89549659be0fb1e5c753ee9a131a42a56
Anuj Gupta <129574489+anujgupta8519@users.noreply.github.com>
2024-02-03 19:14:09 +0530

    Componets are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+
 import './App.css'
+import authService from './appwrite/auth';
+import { login, logout } from './store/features/authSlice';
+import { Footer, Header } from './components';
 
 function App() {
-console.log()
+const [loading,setLoading] = useState(true);
+const dispatch =  useDispatch();
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if (userData) {
+      dispatch(login({userData}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>{
+    setLoading(false)
+  })
 
-  return (
-<>
-Hello</>
-  )
+
+},[])
+
+
+return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className="w-full block">
+    <Header/>
+    <main>
+   {/* <Outlet/> */}
+    </main>
+    <Footer/>
+    </div>
+  </div>
+):null;
 }
 
 export default App
