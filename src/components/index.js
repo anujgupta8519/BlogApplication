87237274a565f6a38890de88809f38481d4205ad
Anuj Gupta <129574489+anujgupta8519@users.noreply.github.com>
2024-02-06 21:45:55 +0530

    AuthLayout,Login,PostCard,Select and Signup done and some changes made in store

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Header from "./Header/Header";
 import Logo from "./Logo";
 import Container from './container/Container'
 import LogoutButton from './Header/LogoutButton'
+import Input from './Input/Input'
+import Button from './Button/Button'
 
-export {Header, Footer,Logo,Container,LogoutButton}
+export {Header, Footer,Logo,Container,LogoutButton,Input,Button}
