652a4e3134f9e3b20a684c9b32e4228f320bd5c0
Anuj Gupta <129574489+anujgupta8519@users.noreply.github.com>
2024-02-10 16:09:09 +0530

    windows object is change

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -11,7 +11,7 @@ function LogoutButton() {
       dispatch(logout())
     })
     .finally(()=>{
-      window.location.reload();
+      window.location.replace("https://bloggerkrishna.netlify.app/");
     })
   }
 
